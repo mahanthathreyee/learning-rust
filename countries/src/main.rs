bca1175736a7d8e0ffe02f5159c09bb8f6f46e5f
Maganth Seetharaman <maganth.seetharaman@freshworks.com>
2020-08-03 03:32:24 +0530

    V1 get request | Successful request | JSON parsing failure

--- countries/src/main.rs
+++ countries/src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use url::{Url, ParseError};
+
+const COUNTRIES_API: &str = "https://restcountries.eu/rest/v2/name";
+
+struct RequestHandler {
+    client: reqwest::Client
+}
+
+impl RequestHandler {
+    pub fn new() -> RequestHandler {
+        RequestHandler {
+            client: reqwest::Client::builder().build().unwrap()
+        }
+    }
+
+    fn get_url(&self, country_name: String) ->Url {
+        let country_url = format!("{}/{}", COUNTRIES_API, country_name.as_str());
+        let country_url = match Url::parse(country_url.as_str()) {
+            Ok(t) => t,
+            Err(_) => panic!("Error constructing url")
+        };
+
+        country_url
+    }
+
+    pub async fn request_country_data(&self, country_name: String) ->Result<HashMap<String,String>, Box<dyn std::error::Error>> {
+        let country_url = self.get_url(country_name);
+        println!("URL = {}", country_url.to_string());
+        let response = match self.client.get(country_url).send().await {
+            Ok(t) => t,
+            Err(_) => panic!("Error contacting country API server")
+        };
+
+        if response.status().is_success() {
+            println!("success!");
+        }
+
+        let response = match response.json::<HashMap<String, String>>().await {
+            Ok(t) => t,
+            Err(_) => panic!("Error parsing json")
+        };
+        Ok(response)
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let request_handler = RequestHandler::new();
+    let response = match request_handler.request_country_data("USA".to_string()).await {
+        Ok(t) => t,
+        Err(_) => panic!("Error")
+    };
+    println!("{:#?}", response);
 }
